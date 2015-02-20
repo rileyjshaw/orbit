c6c678adc3240db145f6c4a34f9a0caa195b8324
Riley Shaw <rileyjshaw@gmail.com>
2015-02-20 00:34:14 -0500

    Update 2015-02-20T05:34:13.055Z

--- README.md
+++
@@ -1,3 +0,0 @@
-# [orbit](http://rileyjshaw.com/orbit)
------
-A game: hit space over the red dots to earn points
--- app/js/dom.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.getElementById('bg');
-var ctx = canvas.getContext('2d');
-var score = document.getElementById('score');
-
-module.exports = {
-  canvas: canvas,
-  ctx: ctx,
-  score: score,
-};
